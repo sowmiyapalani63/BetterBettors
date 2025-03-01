9bd2a493d1315f6c2d6160c4810c26bc926fd48f
Murali <dmurali13032001>
2025-03-01 16:09:55 +0530

    "header replaced"

---
+++ Better/Better/script.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let carousel = document.querySelector("#horseCarousel");
+//     let items = carousel.querySelectorAll(".carousel-item");
+
+//     items.forEach((el) => {
+//         const next = el.nextElementSibling ? el.nextElementSibling : items[0];
+//         next.children[0].children[0].cloneNode(true);
+//         el.appendChild(next.children[0].cloneNode(true));
+//     });
+// });
